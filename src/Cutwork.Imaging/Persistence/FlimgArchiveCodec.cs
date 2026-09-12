26112a10fd2951c2fce83d9c6496c8b0c17774cf
Akino Nanto <akino.nanto@icloud.com>
2026-09-12 22:46:12 +0900

    fix: declare persistence IO namespace

--- src/Cutwork.Imaging/Persistence/FlimgArchiveCodec.cs
+++ src/Cutwork.Imaging/Persistence/FlimgArchiveCodec.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
--- src/Cutwork.Imaging/Persistence/PngAssetCodec.cs
+++ src/Cutwork.Imaging/Persistence/PngAssetCodec.cs
@@ -1,4 +1,5 @@
 using System.Buffers.Binary;
+using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Flamoris.Cutwork.Core;
